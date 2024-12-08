395e86cd979f6b80a2608751e111576ac3357da4
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-08 16:47:29 +0900

    firstCommit

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,11 @@
-import type { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const authConfig: NextAuthConfig = {
   providers: [
     Google, // たったこれだけ
   ], // 後ほどここにGoogle プロバイダを追加していきます
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async jwt({ token, user, account }) {
       if (user && account?.id_token) {
